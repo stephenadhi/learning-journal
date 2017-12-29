608f6ce4fd91022a11930350514dceb53f1a2c18
stephenadhi <stephenadhi@gmail.com>
2017-12-29 17:46:28 +0100

    back end

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/superuser/Desktop/programming/python_work/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
